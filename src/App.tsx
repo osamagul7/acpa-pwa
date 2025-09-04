5670640d16033a313ce0f2f880727f3040bb115e
usama.gul <usama.gul@iengineering.com>
2025-09-04 17:19:18 +0500

    intial setup

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+const App: React.FC = () => {
+  React.useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then((reg) => console.log("SW registered", reg))
+          .catch((err) => console.error("SW registration failed", err));
+      });
+    }
+  }, []);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+// Home page component for displaying main landing page content
+import React from "react";
+
+const Home: React.FC = () => {
+  return <div>home</div>;
+};
+
+export default Home;
